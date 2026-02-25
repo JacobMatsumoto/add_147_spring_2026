6fe024e2ce72e49ec9ea68948a7e3224ff4356de
Jacob <jmatsumoto@students.mchenry.edu>
2026-02-24 23:00:47 -0600

    homework done

--- hot_cold_game/scripts/script.js
+++ hot_cold_game/scripts/script.js
@@ -1,5 +1,5 @@
 //Addapted heavily and unneccassarily from the hangman project (probably made it more pain that it was worth) as this uses sort of completely different logic
-let game = Math.floor(Math.random() * 1000);
+let game = Math.floor((Math.random() * 1000) + 1);
 let answer = game;
 let output = '';
 let user_num = '';
@@ -18,41 +18,41 @@ document.getElementById("submit").addEventListener("click", function(event){
     output = '';
     user_num = document.getElementById("guess").value;
     document.getElementById("guess").value = '';
-    comparison = Math.abs(user_num - answer) //https://www.w3schools.com/js/js_math.asp
+    comparison = Math.abs(user_num - answer); //https://www.w3schools.com/js/js_math.asp
 
     if (user_num == answer) {
         found = true;
         document.getElementById("guesses").innerHTML = 'YOU WIN!!!';
-        document.getElementById("hot_or_cold").innerHTML = "You got it!!!"
-        document.getElementById("hot_or_cold").style.color = "red"
+        document.getElementById("hot_or_cold").innerHTML = "You got it!!!";
+        document.getElementById("hot_or_cold").style.color = "red";
         }
     else if (comparison <= 5) {
-        document.getElementById("hot_or_cold").innerHTML = "On fire!"
-        document.getElementById("hot_or_cold").style.color = "red"
+        document.getElementById("hot_or_cold").innerHTML = "On fire!";
+        document.getElementById("hot_or_cold").style.color = "red";
     }else if (comparison <= 10) {
-        document.getElementById("hot_or_cold").innerHTML = "Very Hot!"
-        document.getElementById("hot_or_cold").style.color = "orange"
+        document.getElementById("hot_or_cold").innerHTML = "Very Hot!";
+        document.getElementById("hot_or_cold").style.color = "orange";
     }else if (comparison <= 50) {
-        document.getElementById("hot_or_cold").innerHTML = "Hot"
-        document.getElementById("hot_or_cold").style.color = "gold"
+        document.getElementById("hot_or_cold").innerHTML = "Hot";
+        document.getElementById("hot_or_cold").style.color = "gold";
     }else if (comparison <= 100) {
-        document.getElementById("hot_or_cold").innerHTML = "warm"
-        document.getElementById("hot_or_cold").style.color = "yellow"
+        document.getElementById("hot_or_cold").innerHTML = "warm";
+        document.getElementById("hot_or_cold").style.color = "yellow";
     }else if (comparison <= 200) {
-        document.getElementById("hot_or_cold").innerHTML = "cool"
-        document.getElementById("hot_or_cold").style.color = "lightblue"
+        document.getElementById("hot_or_cold").innerHTML = "cool";
+        document.getElementById("hot_or_cold").style.color = "lightblue";
     }else if (comparison <= 300) {
-        document.getElementById("hot_or_cold").innerHTML = "cold"
-        document.getElementById("hot_or_cold").style.color = "blue"
+        document.getElementById("hot_or_cold").innerHTML = "cold";
+        document.getElementById("hot_or_cold").style.color = "blue";
     }else if (comparison <= 500) {
-        document.getElementById("hot_or_cold").innerHTML = "very cold"
-        document.getElementById("hot_or_cold").style.color = "purple"
+        document.getElementById("hot_or_cold").innerHTML = "very cold";
+        document.getElementById("hot_or_cold").style.color = "purple";
     }else if (comparison <= 750) {
-        document.getElementById("hot_or_cold").innerHTML = "insanely cold"
-        document.getElementById("hot_or_cold").style.color = "darkgrey"
+        document.getElementById("hot_or_cold").innerHTML = "insanely cold";
+        document.getElementById("hot_or_cold").style.color = "darkgrey";
     }else {
-        document.getElementById("hot_or_cold").innerHTML = "frozen"
-        document.getElementById("hot_or_cold").style.color = "black"
+        document.getElementById("hot_or_cold").innerHTML = "frozen";
+        document.getElementById("hot_or_cold").style.color = "black";
     }
 
   
--- learn10-start/scripts/learn10.js
+++ learn10-start/scripts/learn10.js
@@ -2,11 +2,34 @@ function start() {
     // Math: https://www.w3schools.com/js/js_math.asp
     // Demonstrate the use of round, ceil, floor, trunc, sign
     // pow, min, and random and display to the appropriate paragraphs
+    let my_float1 = 4.4;
+    let my_float2 = 4.6;
+    let my_round = Math.round(my_float1)
+    let my_ceil = Math.ceil(my_float1)
+    let my_floor = Math.floor(my_float2)
+    let my_trunc = Math.trunc(my_float2)
+    let my_sign = Math.sign(my_float1)
+    let my_pow = Math.pow(2, 5)
+    let my_min = Math.min(my_float1, my_float2, my_pow)
+    let my_rand = Math.random()
+
+    document.getElementById("round").innerHTML = my_round
+    document.getElementById("ceil").innerHTML = my_ceil
+    document.getElementById("floor").innerHTML = my_floor
+    document.getElementById("trunc").innerHTML = my_trunc
+    document.getElementById("sign").innerHTML = my_sign
+    document.getElementById("pow").innerHTML = my_pow 
+    document.getElementById("min").innerHTML = my_min
+    document.getElementById("random").innerHTML = my_rand
+
+
+
 
 
     // Random: https://www.w3schools.com/js/js_random.asp
     // create a random integer between 1 and 100 and display in the random2 paragraph
-
+    let my_random_1_to_100 = Math.floor((Math.random() * 100) + 1)
+    document.getElementById("random2").innerHTML = my_random_1_to_100
 
     // Booleans: https://www.w3schools.com/js/js_booleans.asp
     // read the reference
@@ -15,7 +38,8 @@ function start() {
     // Comparisons: https://www.w3schools.com/js/js_comparisons.asp
     // demonstrate and explain the difference between == and === in the
     // comparisons paragraph
-
+    let my_understanding = "From my understanding, === is a very strict comparison, 1 the int is not equal to \"1\" the string. However, under a == comparison 1==\"1\" it returns true because it more so just sees well, these are both 1 it doesn't take into account one is a string while the other is an int. Whereas === also checks type. ! then replaces the first of the =s and makes it a does not equal operator == loose comparison, === strict comparison (Same question from learn07?) Continued, == is just value, === is both value and type comparison"
+    document.getElementById("comparisons").innerHTML = my_understanding
 
 
     // Conditions: https://www.w3schools.com/js/js_if_else.asp
